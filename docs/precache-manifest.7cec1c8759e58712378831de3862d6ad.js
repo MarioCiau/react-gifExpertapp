d415546d19391613513fa9acf20e91da0099b5f6
Mario Ciau <mario_josue1110@hotmaol.com>
2021-08-25 22:50:47 -0500

    Docs agregado

---
+++ docs/precache-manifest.7cec1c8759e58712378831de3862d6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69be951d6927a43ded941ec3881419b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19454921e63abe2704bc",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19454921e63abe2704bc",
+    "url": "/static/js/main.09bfa1ed.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
